bc9a9525831559398a7dbca70b94125a54c428a3
Paulina Moreno <elizabeth.moreno.aguilera@gmail.com>
2020-06-17 19:08:07 -0700

    Adding credential variables

--- tf_modules/azure/main.tf
+++ tf_modules/azure/main.tf
@@ -1,5 +1,13 @@
+variable "client_secret" {}
+variable "client_id" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+
 provider "azurerm" {
-  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 variable "prefix" {
